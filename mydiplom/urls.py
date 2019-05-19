3f55a811f27e2b3174546fbe3e80a7996629544a
Aleksandra <you@example.com>
2019-05-19 14:16:07 +0300

    change base.html

--- mydiplom/urls.py
+++ mydiplom/urls.py
@@ -5,6 +5,3 @@ urlpatterns = [
 	path('admin/', admin.site.urls),
 	path('', include('diplom.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
