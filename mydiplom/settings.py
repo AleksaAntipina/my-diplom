bffdd21bcf662283abe4fbe5469fe3cd6760affc
Aleksandra <you@example.com>
2019-05-19 15:41:13 +0300

    add sorting when u tab on the column name

--- mydiplom/settings.py
+++ mydiplom/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$cns$a+zl=2c07c1equ4kwt(tyjs4!6@hl4@#zli507s$$r(op'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'AleksaAntipina.pythonanywhere.com']
 
